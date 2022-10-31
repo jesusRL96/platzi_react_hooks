4d6fd4237d6a2b64fa1384fb0643f0a9bbb764cd
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-10-31 09:54:48 -0600

    useRef: manejo profesional de inputs y formularios

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useMemo} from 'react';
+import React, { useState, useEffect, useReducer, useMemo, useRef} from 'react';
 
 const initialState = {
 	favorites: [],
@@ -20,6 +20,7 @@ const Characters = () => {
 	const [characters, setCharacters] = useState([]);
 	const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 	const [search, setSearch] = useState('');
+	const searchInput = useRef(null);
 
 	useEffect(() => {
 		fetch('https://rickandmortyapi.com/api/character/')
@@ -33,8 +34,8 @@ const Characters = () => {
 		
 	}
 
-	const handleSearch = (event) => {
-		setSearch(event.target.value);
+	const handleSearch = () => {
+		setSearch(searchInput.current.value);
 	}
 
 	/* const filteredUsers = characters.filter((user) => { */
@@ -56,7 +57,7 @@ const Characters = () => {
 			))}
 
 			<div className="search">
-				<input type="text" name="search" id="search" onChange={handleSearch} />
+				<input type="text" name="search" id="search" ref={searchInput} onChange={handleSearch} />
 			</div>
 
 			{filteredUsers.map(character => (
